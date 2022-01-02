82f2c129c34b3f1cc4f1091039676730a24d7160
rs21 <rayturla21@yahoo.com>
2022-01-02 23:02:50 +0800

    Added More to CodeBlock shareables content

--- src/app/shareables/components/code-block/code-block.component.ts
+++ src/app/shareables/components/code-block/code-block.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { JsonService } from '../../../services/json.service'
 @Component({
   selector: 'app-code-block',
   templateUrl: './code-block.component.html',
@@ -7,38 +7,13 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CodeBlockComponent implements OnInit {
   @Input() code:string
-  index = `
-    import { configServer } from \"@mayajs/core\";
-    import { AppModule } from \"./app.module\";
-    import cors from \"cors\";
-    import bodyParser from \"body-parser\";
-
-    configServer(3333)
-      .usePlugins([
-        bodyParser,
-        cors
-        // Add plugins or middlewares here
-        // i.e. CORS, body-parser, multer and etc...
-      ])
-      .bootstrapModule(AppModule);
-  `
-  service = `
-    import { Service } from \"@mayajs/core\";
-    import { MongoDbServices } from \"@mayajs/mongo\";
-
-    @Service()
-    export class SampleServices {
-      get model() {
-        const db = this.mongo.database(\"main\");
-        return db.instance.model(\"Sample\");
-      }
-
-      constructor(private mongo: MongoDbServices) {}
-    }
-  `
-  constructor() { }
-
+  constructor(private json: JsonService) { }
+  directory;
+  sourceCode;
   ngOnInit(): void {
+    const {directory, srcCode} = this.json.data
+    this.directory = directory
+    this.sourceCode = srcCode
   }
 
 }
