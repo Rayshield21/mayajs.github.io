7165e3c489709f4328f38e4e5abc5f6c8da8094f
rs21 <rayturla21@yahoo.com>
2022-01-02 23:03:23 +0800

    Refactor MayaJS text highlight shareable component

--- src/app/shareables/components/maya-txt/maya-txt.component.ts
+++ src/app/shareables/components/maya-txt/maya-txt.component.ts
@@ -1,42 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { highlights } from 'src/lang/fil';
 @Component({
-  selector: 'app-maya-txt',
+  selector: 'app-maya-text',
   templateUrl: './maya-txt.component.html',
   styleUrls: ['./maya-txt.scss']
 })
 export class mayaTxtComponent implements OnInit {
   @Input() mayaTxt: string
-  mayaTerms = [
-    {
-      name: "MayaJS",
-      switchValue: 'maya'
-    },
-    {
-      name: "@mayajs/core",
-      switchValue: 'core'
-    },
-    {
-      name: "@mayajs/common",
-      switchValue: 'common'
-    },
-    {
-      name: "@mayajs/router",
-      switchValue: 'router'
-    },
-    {
-      name: "MayaJS CLI",
-      switchValue: 'cli'
-    },
-    {
-      name: "@mayajs/mongo",
-      switchValue: 'mongo'
-    },
-    {
-      name: "@mayajs/sql",
-      switchValue: 'sql'
-    },
-  ]
+  highlights = highlights
   constructor() { }
 
   ngOnInit(): void {
